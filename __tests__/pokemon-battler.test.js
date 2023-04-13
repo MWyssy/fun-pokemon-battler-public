edc2d0bc5957831140295b8c4910e79035be67fa
Suzi Shrestha Best <suzi@Suzis-MacBook-Air.local>
2023-04-13 13:33:06 +0100

    worked on class pokemon battler

--- __tests__/pokemon-battler.test.js
+++ __tests__/pokemon-battler.test.js
@@ -43,5 +43,52 @@ describe('pokemonBattler Tests', () => {
                 expect(pikachu.move).toBe(expectedOutput);
             });
         });
+        describe.only('pokemon methods', () => {
+            test('should have a take damage method that takes a number and reduces health by the number given', () => {
+                //Arrange
+                const input = 'Pikachu'
+                const expectedOutput = 30;  
+                const pikachu = new Pokemon(input);
+                //Act
+                pikachu.takeDamage(20)
+
+                //Assert
+                expect(pikachu.hitPoints).toBe(expectedOutput);
+            });
+
+            test('should have a take damage method that takes a number and reduces health by the number given', () => {
+                //Arrange
+                const consoleSpy = jest.spyOn(console,'log')
+                const input = 'Pikachu'
+                const expectedOutput = 20;  
+                const pikachu = new Pokemon(input);
+                //Act
+
+                //Assert
+                expect(pikachu.useMove()).toBe(expectedOutput);
+                expect(consoleSpy).toHaveBeenCalledWith("Pikachu used Pikachu's move")
+            });
+
+            test('should have a has fainted method that returns a boolen based on whether the pokeman has fainted or not', () => {
+                //Arrange
+                const input = 'Pikachu' 
+                const pikachu = new Pokemon(input);
+                //Act
+
+                //Assert
+                expect(pikachu.hasFainted()).toBe(false);
+                //Act
+                pikachu.takeDamage(50)
+
+                //Assert
+                expect(pikachu.hasFainted()).toBe(true);
+                
+
+
+                
+            });
+            
     });
+
   });
+});
--- pokemon-battler.js
+++ pokemon-battler.js
@@ -5,6 +5,20 @@ class Pokemon {
         this.attackDamage = 20;
         this.move = 'tackle';
     };
+    takeDamage(damage) {
+        return this.hitPoints -= damage
+    }
+    useMove() {
+        console.log(`${this.name} used ${this.name}'s move`)
+        return this.attackDamage
+    }
+
+    hasFainted() {
+        if(this.hitPoints === 0) return true;
+        return false;
+        
+    }
 };
+
 
 module.exports = Pokemon;
